d6ceaaf56cf946ef148a95dc7163a9596384dcc9
skiwi <frank_skiwi@hotmail.com>
2015-02-23 21:09:16 +0100

    Card data gets updated when an entity changes zone.

--- src/main/java/com/github/skiwi2/hearthmonitor/commands/TransitioningCommand.java
+++ src/main/java/com/github/skiwi2/hearthmonitor/commands/TransitioningCommand.java
@@ -6,14 +6,17 @@ import com.cardshifter.modapi.base.ECSGame;
 import com.cardshifter.modapi.base.Entity;
 import com.cardshifter.modapi.resources.ECSResourceMap;
 import com.cardshifter.modapi.resources.ResourceRetriever;
+import com.github.skiwi2.hearthmonitor.CardData;
 import com.github.skiwi2.hearthmonitor.logapi.power.CardEntityLogObject;
 import com.github.skiwi2.hearthmonitor.logapi.power.EntityLogObject;
 import com.github.skiwi2.hearthmonitor.logapi.power.PlayerEntityLogObject;
 import com.github.skiwi2.hearthmonitor.logapi.zone.TransitioningLogEntry;
+import com.github.skiwi2.hearthmonitor.model.CardDataComponent;
 import com.github.skiwi2.hearthmonitor.model.HearthStoneMod.HearthStoneAttribute;
 import com.github.skiwi2.hearthmonitor.model.HearthStoneMod.HearthStoneResource;
 
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Command to execute the TransitioningLogEntry on a game.
@@ -25,6 +28,7 @@ public class TransitioningCommand extends AbstractCommand {
     private final TransitioningLogEntry transitioningLogEntry;
 
     private String oldZone;
+    private Optional<CardDataComponent> oldCardDataComponent;
 
     /**
      * Constructs a new TransitioningCommand instance.
@@ -60,6 +64,17 @@ public class TransitioningCommand extends AbstractCommand {
         AttributeRetriever attributeRetriever = AttributeRetriever.forAttribute(HearthStoneAttribute.ZONE);
         oldZone = attributeRetriever.getFor(logEntity);
         attributeRetriever.attrFor(logEntity).set(transitioningLogEntry.getTargetZone());
+
+        oldCardDataComponent = Optional.ofNullable(logEntity.hasComponent(CardDataComponent.class) ? logEntity.getComponent(CardDataComponent.class) : null);
+        if (transitioningLogEntry.getEntity() instanceof CardEntityLogObject) {
+            String cardId = ((CardEntityLogObject)transitioningLogEntry.getEntity()).getCardId();
+            if (!cardId.isEmpty()) {
+                if (logEntity.hasComponent(CardDataComponent.class)) {
+                    logEntity.removeComponent(CardDataComponent.class);
+                }
+                logEntity.addComponent(new CardDataComponent(CardData.getForCardId(cardId)));
+            }
+        }
     }
 
     @Override
@@ -80,5 +95,10 @@ public class TransitioningCommand extends AbstractCommand {
             .findFirst().get();
         AttributeRetriever attributeRetriever = AttributeRetriever.forAttribute(HearthStoneAttribute.ZONE);
         attributeRetriever.attrFor(logEntity).set(oldZone);
+
+        if (logEntity.hasComponent(CardDataComponent.class)) {
+            logEntity.removeComponent(CardDataComponent.class);
+        }
+        oldCardDataComponent.ifPresent(logEntity::addComponent);
     }
 }
