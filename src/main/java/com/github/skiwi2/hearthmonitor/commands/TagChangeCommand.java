bb21c96dc071596f9a32a3b74f10ff4bddb72abe
skiwi <frank_skiwi@hotmail.com>
2015-02-24 14:40:06 +0100

    Implemented TagChangeCommand.

--- src/main/java/com/github/skiwi2/hearthmonitor/commands/TagChangeCommand.java
+++ src/main/java/com/github/skiwi2/hearthmonitor/commands/TagChangeCommand.java
@@ -1,7 +1,18 @@
 package com.github.skiwi2.hearthmonitor.commands;
 
+import com.cardshifter.modapi.attributes.AttributeRetriever;
+import com.cardshifter.modapi.attributes.ECSAttribute;
+import com.cardshifter.modapi.attributes.ECSAttributeMap;
 import com.cardshifter.modapi.base.ECSGame;
+import com.cardshifter.modapi.base.Entity;
+import com.cardshifter.modapi.resources.ECSResource;
+import com.cardshifter.modapi.resources.ECSResourceMap;
+import com.cardshifter.modapi.resources.ResourceRetriever;
+import com.github.skiwi2.hearthmonitor.logapi.power.CardEntityLogObject;
+import com.github.skiwi2.hearthmonitor.logapi.power.EntityLogObject;
+import com.github.skiwi2.hearthmonitor.logapi.power.PlayerEntityLogObject;
 import com.github.skiwi2.hearthmonitor.logapi.power.TagChangeLogEntry;
+import com.github.skiwi2.hearthmonitor.model.HearthStoneMod;
 
 import java.util.Objects;
 
@@ -13,6 +24,9 @@ import java.util.Objects;
 public class TagChangeCommand extends AbstractCommand {
     private final ECSGame ecsGame;
     private final TagChangeLogEntry tagChangeLogEntry;
+
+    private int oldResourceValue;
+    private String oldAttributeValue;
 
     /**
      * Constructs a new TagChangeCommand instance.
@@ -28,11 +42,72 @@ public class TagChangeCommand extends AbstractCommand {
 
     @Override
     protected void executeImpl() {
+        ResourceRetriever entityIdRetriever = ResourceRetriever.forResource(HearthStoneMod.HearthStoneResource.ENTITY_ID);
+        Entity logEntity = ecsGame.findEntities(entity -> (entity.hasComponent(ECSResourceMap.class) && entity.hasComponent(ECSAttributeMap.class)))
+            .stream()
+            .filter(entity -> {
+                int entityId = entityIdRetriever.getFor(entity);
+                EntityLogObject entityLogObject = tagChangeLogEntry.getEntity();
+                if (entityLogObject instanceof PlayerEntityLogObject) {
+                    return false;
+                }
+                CardEntityLogObject cardEntityLogObject = (CardEntityLogObject)entityLogObject;
+                int tagChangeEntityId = Integer.parseInt(cardEntityLogObject.getId());
+                return (entityId == tagChangeEntityId);
+            })
+            .findFirst().orElse(null);
+        if (logEntity == null) {
+            return; //TODO fix when ActionStartLogEntry entries are getting processed
+        }
 
+        String tag = tagChangeLogEntry.getTag();
+        String value = tagChangeLogEntry.getValue();
+        if (HearthStoneMod.isHearthStoneResource(tag)) {
+            ECSResource resource = HearthStoneMod.getHearthStoneResource(tag);
+            ResourceRetriever resourceRetriever = ResourceRetriever.forResource(resource);
+            oldResourceValue = resourceRetriever.getOrDefault(logEntity, 0);
+            resourceRetriever.resFor(logEntity).set(Integer.parseInt(value));  //TODO catch NPE?
+        } else if (HearthStoneMod.isHearthStoneAttribute(tag)) {
+            ECSAttribute attribute = HearthStoneMod.getHearthStoneAttribute(tag);
+            AttributeRetriever attributeRetriever = AttributeRetriever.forAttribute(attribute);
+            oldAttributeValue = attributeRetriever.getOrDefault(logEntity, "");
+            attributeRetriever.attrFor(logEntity).set(value);
+        } else {
+            System.out.println("Tag " + tag + " matches neither a resource nor an attribute.");
+        }
     }
 
     @Override
     protected void undoImpl() {
+        ResourceRetriever entityIdRetriever = ResourceRetriever.forResource(HearthStoneMod.HearthStoneResource.ENTITY_ID);
+        Entity logEntity = ecsGame.findEntities(entity -> (entity.hasComponent(ECSResourceMap.class) && entity.hasComponent(ECSAttributeMap.class)))
+            .stream()
+            .filter(entity -> {
+                int entityId = entityIdRetriever.getFor(entity);
+                EntityLogObject entityLogObject = tagChangeLogEntry.getEntity();
+                if (entityLogObject instanceof PlayerEntityLogObject) {
+                    return false;
+                }
+                CardEntityLogObject cardEntityLogObject = (CardEntityLogObject)entityLogObject;
+                int tagChangeEntityId = Integer.parseInt(cardEntityLogObject.getId());
+                return (entityId == tagChangeEntityId);
+            })
+            .findFirst().orElse(null);
+        if (logEntity == null) {
+            return; //TODO fix when ActionStartLogEntry entries are getting processed
+        }
 
+        String tag = tagChangeLogEntry.getTag();
+        if (HearthStoneMod.isHearthStoneResource(tag)) {
+            ECSResource resource = HearthStoneMod.getHearthStoneResource(tag);
+            ResourceRetriever resourceRetriever = ResourceRetriever.forResource(resource);
+            resourceRetriever.resFor(logEntity).set(oldResourceValue);
+        } else if (HearthStoneMod.isHearthStoneAttribute(tag)) {
+            ECSAttribute attribute = HearthStoneMod.getHearthStoneAttribute(tag);
+            AttributeRetriever attributeRetriever = AttributeRetriever.forAttribute(attribute);
+            attributeRetriever.attrFor(logEntity).set(oldAttributeValue);
+        } else {
+            System.out.println("Tag " + tag + " matches neither a resource nor an attribute.");
+        }
     }
 }
